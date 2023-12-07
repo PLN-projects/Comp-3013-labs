7e7712eba88de48913292e98642ae927d175d0bd
PLN-projects <81396770+PLN-projects@users.noreply.github.com>
2023-12-06 19:55:35 -0800

    Final Exam
    Final Examp submitted at 7:55pm pst, I was able to get the filter working but not the highlight

---
+++ final-exam/react-filter/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import { useState } from 'react'
+import { foods } from './foods'
+
+function App() {
+  const [searchItem, setSearchItem] = useState("")
+  function handleSearch(e, foodItem){
+    e.preventDefault()
+    setSearchItem(foodItem.toLowerCase())
+  }
+
+  return (
+    <>
+      <div>
+        <div>
+          <form className='food-form'>
+            <label htmlFor="item">Search Recipe:</label>
+            <input onChange={(e) => handleSearch(e, e.target.value)} type="text" id="item" name='item' />
+          </form>
+        </div>
+        <ul className='food-list'>
+          {foods.filter(foodName => foodName.name.toLowerCase().includes(searchItem)).map(filteredFoods => (
+            <li key={filteredFoods.id}>
+              {/* use string replace to replace  searchItem with searchItem in a mark tag, for some reason it always shows as plain text though an I'm not sure why...*/}
+              <span>{filteredFoods.name}</span>
+              {/* {searchItem.length > 0 ? <span>{filteredFoods.name.replace(searchItem, `<mark>${searchItem}</mark>`)}</span> : <span>{filteredFoods.name}</span>}  */}
+              <span>{filteredFoods.description}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
